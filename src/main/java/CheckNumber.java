7b273007f630588666f82cea7c1d51d67dd571bd
Numero Uno <numerounodeveloper@gmail.com>
2022-10-31 18:33:01 -0400

    Initial commit

---
+++ src/main/java/CheckNumber.java
@@ -0,0 +1,10 @@
+public class CheckNumber {
+    public boolean isEven(int i) {
+        if (i % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
